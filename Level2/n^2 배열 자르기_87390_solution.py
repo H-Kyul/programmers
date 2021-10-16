7dbb46f5431c66d178d9b3db5164f73b8668326f
H-Kyul <71064836+H-Kyul@users.noreply.github.com>
2021-10-16 22:27:31 +0900

    Update n^2 배열 자르기_87390_solution.py

--- Level2/n^2 배열 자르기_87390_solution.py
+++ Level2/n^2 배열 자르기_87390_solution.py
@@ -1,3 +1,19 @@
+# 최종 : 필요한 부분(자르는 부분)만 계산 
+def solution(n, left, right):
+    arr = [max(divmod(x,n))+1 for x in range (left,right+1)]
+    
+    return arr
+
+
+
+
+# 실패코드2 : 시간초과 (문제점? 아마 전체 배열을 구하고 잘랐기 때문)
+def solution(n, left, right):
+    arr = [max(divmod(x,n))+1 for x in range (n*n)]
+    
+    return arr[left:right+1]
+
+
 # 실패 코드1 : 시간초과 (1,4~8만 성공)
 def solution(n, left, right):
     from functools import reduce
