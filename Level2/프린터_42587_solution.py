2957ca0b9d9c8a26e82a5534386217e04b7ff531
Hankyul Kim <71064836+H-Kyul@users.noreply.github.com>
2021-11-13 19:57:27 +0900

    Update 프린터_42587_solution.py

--- Level2/프린터_42587_solution.py
+++ Level2/프린터_42587_solution.py
@@ -9,18 +9,18 @@ def solution(priorities, loc):
         if len(pd) == 1:
             cnt += 1
             break
-        p,o = pd[0], od[0]
-        if p != max(pd):
+        
+        p,o,top_pri = pd[0], od[0], max(pd)
+        pd.popleft()
+        od.popleft()
+
+        if p != top_pri:
             pd.append(p)
             od.append(o)
         else:
-            if o != loc:
-                cnt += 1
-            else:
-                cnt += 1
+            cnt += 1
+            if o == loc:
                 break
-        pd.popleft()
-        od.popleft()
 
     return cnt
     
@@ -32,8 +32,8 @@ def solution(priorities, loc):
     문제풀이 과정
     - STEP1 : 프린터에 대기 중인 문서가 1개라면 > cnt + 1 추가하고 반복문 종료
     - STEP2 : 여러 개의 대기 문서
-        - STEP 2-1 : 1번 대기 문서가 중요도 1순위가 아니라면 > 대기열 가장 뒤로 이동(append, popleft)
+        - STEP 2-1 : 1번 대기 문서가 중요도 1순위가 아니라면 > 대기열 가장 뒤로 이동(popleft,append)
         - STEP 2-2 : 1번 대기 문서가 중요도 1순위 > 내가 요청한 문서가 출력될 때까지 출력,카운트 반복(popleft, cnt+=1)
-              내가 요청한 문서가 출력될 차례면, cnt + 1 후 반복 종료
+              요청한 문서가 출력될 차례면, 반복 종료
     - STEP3 : cnt(출력된 순서) 반환               
     '''
